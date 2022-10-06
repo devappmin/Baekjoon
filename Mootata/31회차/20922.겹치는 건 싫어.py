9bef9eb1c3e01480c0d89014f96760528941feb7
Mootata <dbslt6872@gmail.com>
2022-10-06 15:55:05 +0900

    Mootata/20922

--- Mootata/31회차/20922.겹치는 건 싫어.py
+++ Mootata/31회차/20922.겹치는 건 싫어.py
@@ -2,9 +2,21 @@ from collections import defaultdict
 
 n, k = map(int, input().split())
 nums = list(map(int, input().split()))
+dic = defaultdict(int)
+answer = 0
+count = 0
 
+left, right = 0, 0
 
-left, right = 0, 1
+while right < n:
+    if dic[nums[right]] < k:
+        dic[nums[right]] += 1
+        count += 1
+        right += 1
+    else:
+        dic[nums[left]] -= 1
+        answer = max(answer, count)
+        count -= 1
+        left += 1
 
-while True:
-    if nums[left] 
+print(max(answer, count))
